e92e716b9d0f50d5a1dc14911e1d44ed00426fc9
Goran Trajanvoic <goran.trajanovic09@gmail.com>
2023-05-18 17:12:30 +0200

    Established basic Socket communication UI<->Nextjs API<->Backend

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const { Queue } = require("bullmq");
 const express = require("express");
 const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 const PORT = 8888;
+
 app.use(express.json());
 
 const myQueue = new Queue("recordScreenshots", {
@@ -24,6 +29,10 @@ app.post("/take_screenshots", async (req, res) => {
     res.status(200);
 });
 
-app.listen(PORT, () =>
+io.on("connection", (socket) => {
+    console.log("New User has connected!");
+});
+
+server.listen(PORT, () =>
     console.log(`Express server running on http://localhost:${PORT}`)
 );
